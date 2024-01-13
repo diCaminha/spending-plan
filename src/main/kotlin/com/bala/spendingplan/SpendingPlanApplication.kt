76cf2d88a1effa5b2d3254c1cb21d6eb611b341f
Denis Caminha <denis@MacBook-Air-de-Denis.local>
2024-01-13 10:55:43 -0300

    add files

---
+++ src/main/kotlin/com/bala/spendingplan/SpendingPlanApplication.kt
@@ -0,0 +1,11 @@
+package com.bala.spendingplan
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpendingPlanApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpendingPlanApplication>(*args)
+}
