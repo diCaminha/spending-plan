6d1c1ba68755f418750238e7060d75142b2da763
Denis Caminha <denis@MacBook-Air-de-Denis.local>
2024-02-29 01:13:39 -0300

    add validator for user get plans

--- src/main/kotlin/com/bala/spendingplan/controllers/PlanController.kt
+++ src/main/kotlin/com/bala/spendingplan/controllers/PlanController.kt
@@ -1,5 +1,6 @@
 package com.bala.spendingplan.controllers
 
+import com.bala.spendingplan.configurations.TokenService
 import com.bala.spendingplan.controllers.dto.CategoryDto
 import com.bala.spendingplan.controllers.dto.NewCategoryDto
 import com.bala.spendingplan.services.PlanService
@@ -13,23 +14,29 @@ import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestHeader
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.client.HttpClientErrorException.Unauthorized
 
 @RestController
 @RequestMapping("/plans")
 class PlanController(
     private val planService: PlanService,
-    private val categoryService: CategoryService
+    private val categoryService: CategoryService,
+    private val tokenService: TokenService
 ) {
 
     @GetMapping
-    fun list(@RequestParam("username", required = false) username: String) : List<PlanView> {
-        if (username != null) {
-            return planService.listByUser(username)
+    fun list(@RequestParam("username", required = false) username: String,
+             @RequestHeader("Authorization") authorizationHeader: String) : ResponseEntity<List<PlanView>> {
+        val token = authorizationHeader.substringAfter("Bearer ")
+        val usernameFromToken = tokenService.extractUsername(token)
+        if (usernameFromToken != username) {
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).body(null)
         }
-        return planService.listAll()
+        return ResponseEntity.status(HttpStatus.OK).body(planService.listByUser(username))
     }
 
     @GetMapping("/{id}")
